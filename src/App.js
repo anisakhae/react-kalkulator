906ac4146ad48e50f3413477c49c5e7962a994e3
anisakhae <anisa.khaerani12@gmail.com>
2022-07-29 16:00:23 +0700

    ahdn

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useReducer } from "react"
+import './styles.css';
+
+function reducer (state, saction){
+
+}
+function App() {
+  const [{ currentOperand, previousOperand, operation}] = useReducer(reducer, {})
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'></div>
+        <div className='current-operand'>{currentOperand} {operation}</div>
+      </div>
+      <button className='span-two'>AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  );
+}
+
+export default App;
